0b484b3c6bda163a91e2a2c03d53649645a61c7c
Omkar <omkar6142@gmail.com>
2025-01-17 22:18:06 +0530

    Added setup.py file to export folder as a module in other projects

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="PVT_Correlations",        # Name of your package
+    # version="0.1",            # Package version
+    packages=find_packages(), # Automatically find sub-packages like "my_package"
+    install_requires=[],      # List dependencies here, if any
+    description="Package to create a PVT Table using various correlations",
+    author="Omkar Kurlekar",
+    # author_email="your.email@example.com",
+    # url="http://example.com",
+)
