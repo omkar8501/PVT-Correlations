4886c5cc14c5adbef4bc6965d114195a5c2f6442
Omkar <omkar6142@gmail.com>
2025-01-18 15:59:31 +0530

    Minor changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="PVT_Correlations",        # Name of your package
     version="0.0.1",            # Package version
-    packages=["pvt_correlations"], # Automatically find sub-packages like "my_package"
+    packages=find_packages(), # Automatically find sub-packages like "my_package"
     install_requires=[],      # List dependencies here, if any
     description="Package to create a PVT Table using various correlations",
     author="Omkar Kurlekar",
